97a26000dc07b019c79d25b3961ad59816d04871
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-07-08 21:28:30 +0530

    youtube-dl

--- bin/network-monitor.sh
+++ bin/network-monitor.sh
@@ -4,5 +4,6 @@ if ifconfig wlan0 | grep -q "inet addr:" ; then
 	sleep 60
 else
 	echo "Network connection down! Attempting reconnection."
-	ifup --force wlan0
+	#ifup --force wlan0
+	sudo ifconfig wlan0 up
 fi
