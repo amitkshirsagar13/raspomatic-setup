ca620b7c7feb1ba81bf37c005de69b0e723b00f6
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2019-08-16 09:33:17 +0530

    Update downloadImage.sh

--- bin/downloadImage.sh
+++ bin/downloadImage.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 storage=$1
+logUrl="$2"
 url="$2XINDEX.jpg"
 start=$3
 end=$4
-
+logStorage${logUrl##*/}
 if [ ! -d "$storage" ]; then
     echo "Creating $storage"
     mkdir $storage
@@ -13,6 +14,6 @@ fi
 while [ $start -lt $end ]
 do
     downloadUrl="${url//XINDEX/$start}"
-    wget $downloadUrl -P $storage > 
+    wget $downloadUrl -P $storage >> $storage/0.$logStorage.log
     true $(( start++ ))
 done
